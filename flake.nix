c8808c41f2a7ea302a2b197f33ee01287b8facc5
ntn2142 <ntn2142@proton.me>
2025-07-27 14:34:33 +0200

    update nix dev shell

--- flake.nix
+++ flake.nix
@@ -3,91 +3,60 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    devShell.${system} = with pkgs;
-      mkShell rec {
-        ###
-        ## Library Packages
-        ###
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+      in
+      {
+        devShells.default = pkgs.mkShell rec {
 
-        xLibs = with pkgs; [
-          xorg.libX11
-          xorg.libXcursor
-          xorg.libXi
-          xorg.libXrandr
-        ];
-        dynLibs = with pkgs;
-          [
-            alsa-lib
-            stdenv.cc.cc.lib
-            udev
-            vulkan-loader
-          ]
-          ++ xLibs;
+          buildInputs =
+            with pkgs;
+            [
+              # Rust
+              (rust-bin.stable.latest.default.override { extensions = [ "rust-src" ]; })
+              pkg-config
+            ]
+            ++ lib.optionals (lib.strings.hasInfix "linux" system) [
+              # for Linux
+              # Audio (Linux only)
+              alsa-lib
+              # Cross Platform 3D Graphics API
+              vulkan-loader
+              # For debugging around vulkan
+              vulkan-tools
+              # Other dependencies
+              libudev-zero
+              xorg.libX11
+              xorg.libXcursor
+              xorg.libXi
+              xorg.libXrandr
+              libxkbcommon
+            ];
 
-        ###
-        ## Executable Packages
-        ###
+          RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+            pkgs.vulkan-loader
+            pkgs.xorg.libX11
+            pkgs.xorg.libXi
+            pkgs.xorg.libXcursor
+            pkgs.libxkbcommon
 
-        buildInputs = with pkgs;
-          [
-            clang
-            # Replace llvmPackages with llvmPackages_X, where X is the latest
-            # LLVM version (at the time of writing, 16)
-            llvmPackages_16.bintools
-            mold
-            pkg-config
-            rustup
-          ]
-          ++ dynLibs;
-
-        ###
-        ## Rust Toolchain Setup
-        ###
-
-        RUSTC_VERSION = pkgs.lib.readFile ./rust-toolchain;
-        shellHook = ''
-          export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
-          export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
-        '';
-
-        ###
-        ## Rust Bindgen Setup
-        ###
-
-        # So bindgen can find libclang.so
-        LIBCLANG_PATH = pkgs.lib.makeLibraryPath [pkgs.llvmPackages_16.libclang.lib];
-        # Add headers to bindgen search path
-        BINDGEN_EXTRA_CLANG_ARGS =
-          # Includes with normal include path
-          (builtins.map (a: ''-I"${a}/include"'') [
-            # add dev libraries here (e.g. pkgs.libvmi.dev)
-            pkgs.glibc.dev
-          ])
-          # Includes with special directory paths
-          ++ [
-            ''-I"${pkgs.llvmPackages_16.libclang.lib}/lib/clang/${pkgs.llvmPackages_16.libclang.version}/include"''
-            ''-I"${pkgs.glib.dev}/include/glib-2.0"''
-            ''-I${pkgs.glib.out}/lib/glib-2.0/include/''
           ];
-
-        ###
-        ## Linking with System libraries
-        ###
-
-        # Add precompiled library to rustc search path
-        RUSTFLAGS = builtins.map (a: ''-L ${a}/lib'') dynLibs;
-
-        # For some reason the Vulkan loader needs to be in the dynamic linker path.
-        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.vulkan-loader];
-      };
-  };
+        };
+      }
+    );
 }
